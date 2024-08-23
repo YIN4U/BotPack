3251c9a398f5df9cb5ef45b3f2d32e5957ce2f2f
YIN4U <moussabenalisalah@gmail.com>
2024-08-23 20:50:23 +0100

    Add files via upload

--- modules/commands/unsend.js
+++ modules/commands/unsend.js
@@ -1,28 +1,27 @@
 module.exports.config = {
-	name: "unsend",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Unsend bot's messages",
-  usePrefix: true,
-	commandCategory: "message",
-	usages: "unsend",
-	cooldowns: 0
+    name: "حذف",
+    version: "1.0.1",
+    hasPermssion: 1,
+    credits: "عمر",
+    description: "ا",
+    commandCategory: "مسؤولي المجموعات",
+    usages: "حذف رسـائل البوت",
+    cooldowns: 0
 };
 
-module.exports.run = function({ api, event, getText }) {
-	if (!event.messageReply) {
-		return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-	}
-
-	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	
-	return api.unsendMessage(event.messageReply.messageID);
+module.exports.languages = {
+    "vi": {
+        "returnCant": "Không thể gỡ tin nhắn của người khác.",
+        "missingReply": "Hãy reply tin nhắn cần gỡ."
+    },
+    "en": {
+        "returnCant": "اقول تدخل حسابه وتحذفها 🙂🗡️",
+        "missingReply": "رد عا رسالتي 🙂"
+    }
 }
 
-module.exports.languages = {
-	"en": {
-		"returnCant": "Can't remove other people's messages.",
-		"missingReply": "You can't unsend a message out of nowhere. Please reply to a message first."
-	}
+module.exports.run = function({ api, event, getText }) {
+    if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+    if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+    return api.unsendMessage(event.messageReply.messageID);
 }
